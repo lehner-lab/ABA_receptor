63df569b1c8dbe57e967c4898523eba440aad4dc
Maximilian Stammnitz <MaximilianStammnitz@users.noreply.github.com>
2025-06-10 21:12:26 +0200

    Add files via upload

--- scripts/FigureS4/FigureS4_all_drcs.R
+++ scripts/FigureS4/FigureS4_all_drcs.R
@@ -368,3 +368,32 @@ dev.off()
 ## 4. Version ##
 ################
 
+# sessionInfo()
+# R version 4.4.1 (2024-06-14)
+# Platform: aarch64-apple-darwin20
+# Running under: macOS Sonoma 14.6.1
+# 
+# Matrix products: default
+# BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib 
+# LAPACK: /Library/Frameworks/R.framework/Versions/4.4-arm64/Resources/lib/libRlapack.dylib;  LAPACK version 3.12.0
+# 
+# locale:
+# [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+# 
+# time zone: Europe/Madrid
+# tzcode source: internal
+# 
+# attached base packages:
+# [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+# [1] wesanderson_0.3.7 cowplot_1.1.3     ggtext_0.1.2      ggplot2_3.5.1     reshape_0.8.9     scales_1.3.0      drc_3.0-1        
+# [8] MASS_7.3-64       stringr_1.5.1    
+# 
+# loaded via a namespace (and not attached):
+# [1] Matrix_1.7-2      gtable_0.3.6      dplyr_1.1.4       compiler_4.4.1    gtools_3.9.5      tidyselect_1.2.1  plotrix_3.8-4    
+# [8] Rcpp_1.0.14       xml2_1.3.6        splines_4.4.1     lattice_0.22-6    TH.data_1.1-3     R6_2.6.1          plyr_1.8.9       
+# [15] generics_0.1.3    Formula_1.2-5     tibble_3.2.1      car_3.1-3         munsell_0.5.1     pillar_1.10.1     rlang_1.1.5      
+# [22] multcomp_1.4-28   stringi_1.8.4     cli_3.6.4         withr_3.0.2       magrittr_2.0.3    gridtext_0.1.5    grid_4.4.1       
+# [29] rstudioapi_0.17.1 mvtnorm_1.3-3     sandwich_3.1-1    lifecycle_1.0.4   vctrs_0.6.5       glue_1.8.0        codetools_0.2-20 
+# [36] zoo_1.8-12        survival_3.8-3    abind_1.4-8       carData_3.0-5     colorspace_2.1-1  pkgconfig_2.0.3   tools_4.4.1   
